a51fa57c36276cf46a79f956e1b88a05c3281f1e
Brandon Valosek <bvalosek@gmail.com>
2021-10-26 14:05:56 -0500

    add realm constraints and config to graph schema

--- src/mapping.ts
+++ src/mapping.ts
@@ -30,8 +30,28 @@ export function handleInfuserAdded(event: InfuserAdded): void { }
 export function handleInfuserRemoved(event: InfuserRemoved): void { }
 
 export function handleRealmCreated(event: RealmCreated): void {
-  const realm = new Realm(event.params.realmId.toString());
+  const realmId = event.params.realmId;
+  const realm = new Realm(realmId.toString());
+
   realm.name = event.params.name;
   realm.description = event.params.description;
+  realm.createdAt = event.block.number;
+  realm.modifiedAt = event.block.number;
+
+  const contract = HyperVIBES.bind(event.address);
+  const config = contract.realmConfig(realmId);
+  const constraints = config.value1;
+
+  realm.token = config.value0.toHexString();
+
+  realm.minDailyRate = constraints.minDailyRate;
+  realm.maxDailyRate = constraints.maxDailyRate;
+  realm.minInfusionAmount = constraints.minInfusionAmount;
+  realm.maxTokenBalance = constraints.maxTokenBalance;
+  realm.requireNftIsOwned = constraints.requireNftIsOwned;
+  realm.allowMultiInfuse = constraints.allowMultiInfuse;
+  realm.allowPublicInfusion = constraints.allowPublicInfusion;
+  realm.allowAllCollections = constraints.allowAllCollections;
+
   realm.save();
 }
